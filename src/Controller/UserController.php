d5ccfc9e475865c6bf0f46f4d8bcfdeadf49d087
pr-olga <frkhorkovaolga@gmail.com>
2020-03-07 13:43:50 +0100

    feat: add user controller

---
+++ src/Controller/UserController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Player;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use App\Repository\PlayerRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations\RequestParam;
+use FOS\RestBundle\Request\ParamFetcher;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserController extends AbstractFOSRestController
+{
+
+    private $userRepository;
+    private $entityManager;
+
+    public function __construct(UserRepository $userRepository, EntityManagerInterface $entityManager)
+    {
+        $this->userRepository = $userRepository;
+        $this->entityManager = $entityManager;
+    }
+
+    public function getUsersAction()
+    {
+        $data = $this->userRepository->findAll();
+        return $this->view($data, Response::HTTP_OK);
+    }
+
+    public function getUserAction(int $id)
+    {
+        $user = $this->userRepository->findOneBy(['id' => $id]);
+        return $this->view($user, Response::HTTP_OK);
+    }
+
+    /**
+     * @RequestParam(name="name")
+     * @param ParamFetcher $paramFetcher
+     */
+    public function postUserAction(ParamFetcher $paramFetcher)
+    {
+        $name = $paramFetcher->get('name');
+
+        if ($name) {
+          $user = new User();
+          $user->setName($name);
+
+          $this->entityManager->persist($user);
+          $this->entityManager->flush();
+
+          return $this->view($user, Response::HTTP_CREATED);
+        }
+
+        return $this->view('error', Response::HTTP_BAD_REQUEST);
+    }
+}
