11e6f40fcc29d77ec8d4f862c3d40a5d72d7d7eb
pr-olga <frkhorkovaolga@gmail.com>
2020-08-02 11:48:17 +0200

    feat: add admin entity

---
+++ src/Entity/Admin.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AdminRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AdminRepository::class)
+ */
+class Admin
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $adminname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $apikey;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAdminname(): ?string
+    {
+        return $this->adminname;
+    }
+
+    public function setAdminname(string $adminname): self
+    {
+        $this->adminname = $adminname;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getApikey(): ?string
+    {
+        return $this->apikey;
+    }
+
+    public function setApikey(?string $apikey): self
+    {
+        $this->apikey = $apikey;
+
+        return $this;
+    }
+}
