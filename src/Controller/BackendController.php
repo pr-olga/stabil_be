be07bfe35ed0466bb65f41856a9aaa25582fd7dc
pr-olga <frkhorkovaolga@gmail.com>
2020-08-02 11:42:01 +0200

    feat: add basic templates: list and single admin

--- src/Controller/BackendController.php
+++ src/Controller/BackendController.php
@@ -3,11 +3,15 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BackendController extends AbstractController
 {
     /**
+     * Show the list of admins
+     *
      * @Route("/backend", name="backend")
      */
     public function index()
@@ -15,5 +19,24 @@ class BackendController extends AbstractController
         return $this->render('backend/index.html.twig', [
             'controller_name' => 'BackendController',
         ]);
+
     }
+
+    /**
+     * Show single admin
+     *
+     * @Route("/backend/{admin?}", name="admin")
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function admin(Request $request)
+    {
+        $name = $request->get('admin');
+
+        return $this->render('backend/admin.html.twig', [
+            'admin_name' => $name
+        ]);
+    }
+
 }
