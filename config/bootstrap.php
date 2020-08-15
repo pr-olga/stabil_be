cd1f7516e4de56c50fa1d3ff26fcc10b25672c95
pr-olga <frkhorkovaolga@gmail.com>
2020-08-15 14:33:29 +0200

    chore: add Procfile

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -3,6 +3,7 @@
 use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
+
 // The check is to ensure we don't use .env in production
 if (!isset($_SERVER['APP_ENV'])) {
     if (!class_exists(Dotenv::class)) {
